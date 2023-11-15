358e4e40a5a3bc7ba93961a635b541f0b4d1d4dd
laxminarayana2911 <pakanatilaxminarayanayadav@gmail.com>
2023-11-15 14:17:43 -0600

    added controller to display one page details

--- controllers/motor.js
+++ controllers/motor.js
@@ -113,4 +113,18 @@ exports.motor_delete = async function(req, res) {
     res.send(`{"error": Error deleting ${err}}`);
     }
    };
+
+   // Handle a show one view with id specified by query
+exports.motor_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await motor.findById( req.query.id)
+    res.render('motordetail', 
+   { title: 'motor Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
    
--- routes/motor.js
+++ routes/motor.js
@@ -4,4 +4,7 @@ var router = express.Router();
 
 /* GET costumes */
 router.get('/', motor_controlers.motor_view_all_Page );
+/* GET detail motor page */
+router.get('/detail', motor_controlers.motor_view_one_Page);
+
 module.exports = router
