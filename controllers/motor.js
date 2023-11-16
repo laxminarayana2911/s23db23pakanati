585b3984966ee737c968488f0ac32f3a169e998a
laxminarayana2911 <pakanatilaxminarayanayadav@gmail.com>
2023-11-15 18:06:45 -0600

    created delete by id controller

--- controllers/motor.js
+++ controllers/motor.js
@@ -155,3 +155,15 @@ exports.motor_update_Page = async function(req, res) {
     res.send(`{'error': '${err}'}`);
     }
     };
+    // Handle a delete one view with id from query
+exports.motor_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await motor.findById(req.query.id)
+    res.render('motordelete', { title: 'Motor Delete', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
--- routes/motor.js
+++ routes/motor.js
@@ -6,9 +6,11 @@ var router = express.Router();
 router.get('/', motor_controlers.motor_view_all_Page );
 /* GET detail motor page */
 router.get('/detail', motor_controlers.motor_view_one_Page);
-/* GET create costume page */
+/* GET create motor page */
 router.get('/create', motor_controlers.motor_create_Page);
 /* GET create update page */
 router.get('/update', motor_controlers.motor_update_Page);
+/* GET delete motor page */
+router.get('/delete', motor_controlers.motor_delete_Page);
 
 module.exports = router
